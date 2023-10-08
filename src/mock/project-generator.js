2d112066cc04690a94fe61836200fb33200f6900
Sriram Pandikasala <psriramajayam@gmail.com>
2023-10-08 18:12:07 +0530

    [SSE] Updated date logic for sprint

--- src/mock/project-generator.js
+++ src/mock/project-generator.js
@@ -4,12 +4,22 @@ function dynamicNumGenerator(max, min = 0) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function convertDaysToMilliseconds(noOfDays) {
+  return noOfDays * 24 * 60 * 60 * 1000;
+}
+
 function offsetDate(max) {
-  return dynamicNumGenerator(max, 10) * 24 * 1000 * 1000;
+  return  convertDaysToMilliseconds(dynamicNumGenerator(max, 10))
 }
 
 function dynamicId() {
   return Date.now() - dynamicNumGenerator(100000);
+}
+
+function addDate(ipDate, offsetDate) {
+  const date =  new Date(ipDate.getTime() + convertDaysToMilliseconds(offsetDate));
+  console.log(ipDate, offsetDate,date)
+  return date 
 }
 
 function randStartDate() {
@@ -31,15 +41,32 @@ export const PROJECT_DUMMY = Array.from(Array(NUMBER_OF_PROJECTS).keys()).map(
       name: `project-${eachProjIndex}`,
     };
 
+    const projStartDate = new Date(randStartDate());
+
+   let prevSprintInfo = {
+      startDate: projStartDate,
+      duration: randDuration()
+    }
+
     proj.sprints = Array.from(
       Array(dynamicNumGenerator(MAX_SPRINT_NUM, 3)).keys()
-    ).map((eachSprintIndex) => {
-      return {
+    ).map((eachSprintIndex, index, ) => {
+      const currSprintInfo = {
+        startDate: addDate(prevSprintInfo.startDate, index ===0 ? 0: prevSprintInfo.duration) ,
+        duration: index === 0 ? prevSprintInfo.duration : randDuration()
+      }
+
+      // console.log(currSprintInfo)
+      const currSprint =  {
         id: `${dynamicId()}`,
         name: `sprint-${eachSprintIndex}`,
-        startDate: `${new Date(randStartDate()).toISOString()}`,
-        duration: `${randDuration()}`,
+        startDate: `${currSprintInfo.startDate.toISOString()}`,
+        duration: `${currSprintInfo.duration}`,
       };
+
+      prevSprintInfo = currSprintInfo;
+
+      return currSprint;
     });
 
     proj.links = [];
